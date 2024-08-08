fea4d4ba785686234eb4f206a029ba35fe82cebd
Alicxshine <alicesoaresdacosta@gmail.com>
2024-08-08 11:30:22 -0300

    v4

--- src/shared/Pizzas.jsx
+++ src/shared/Pizzas.jsx
@@ -11,24 +11,28 @@ const Pizzas = [
       "descricao": "Molho de tomate e mussarela.",
       "imagem": mussarela
     },
+
     {
       "id": 2,
       "nome": "Frango com Catupiry",
       "descricao": "Molho de tomate, mussarela, frango desfiado e catupiry.",
       "imagem": frango
     },
+
     {
       "id": 3,
       "nome": "Portuguesa",
       "descricao": "Molho de tomate, mussarela, presunto, ovo, cebola, azeitona e orégano.",
       "imagem": portuguesa
     },
+
     {
       "id": 4,
       "nome": "Calabresa",
       "descricao": "Molho de tomate, mussarela, calabresa e cebola.",
       "imagem": calabresa
     },
+    
     {
       "id": 5,
       "nome": "Atum",
